c6a303c2baf3a853c8e0ead773f48b0419c6879f
dreyhiflden <dreyhiflden@gmail.com>
2023-01-26 16:58:52 +0200

    module-2 / task 1 / add solution

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,22 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let sum = 0;
+
+  if (Object.keys(salaries).length === 0) {
+    return sum;
+  }
+
+  return Object.values(salaries).reduce(
+    (acc, item) => {
+      if (isSafeNumberValue(item)) {
+        return sum = acc + item;
+      }
+
+      return sum;
+    },
+    sum
+  );
+}
+
+function isSafeNumberValue(value) {
+  return typeof value === 'number' && !Number.isNaN(value) && Number.isFinite(value);
 }
