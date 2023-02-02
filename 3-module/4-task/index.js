4f362a19e72ea86ed7c40a3b0e77ef8ebea92f82
dreyhiflden <dreyhiflden@gmail.com>
2023-02-02 20:43:55 +0200

    module-3 / task 4 / minor changes after I check better solutions

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,10 +1,6 @@
 function showSalary(users, age) {
   return users
     .filter(({ age: userAge }) => userAge <= age)
-    .reduce(
-      (acc, { name, balance }, index, arr) => arr.length - 1 === index
-        ? acc.concat(`${name}, ${balance}`)
-        : acc.concat(`${name}, ${balance}\n`),
-      ''
-    );
+    .map(({ name, balance }) => `${name}, ${balance}`)
+    .join('\n');
 }
